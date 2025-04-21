943de8e59143517bd046d7e9344b2a2fe64550ed
justinmeredith <jm.4444@icloud.com>
2025-04-20 22:37:01 -0500

    Adds the compiled program. Prompts user for a file name, then outputs each line of text from that file. Just showing that the file reading is working properly. This program will not have error checking for the scope of this project.

--- counting_unique_strings.cpp
+++ counting_unique_strings.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <fstream>
 
 using namespace std;
 
@@ -9,6 +10,15 @@ struct UniqueString {
 };
 
 int main() {
+    string file_name;
+    cout << "Enter the name of your text file, including the file extension: ";
+    cin >> file_name;
+    ifstream input_file(file_name);
+    string current_line;
 
+    for (int index = 0; index < 50; index++) {
+        getline(input_file, current_line);
+        cout << current_line << endl;
+    }
     return 0;
 }
