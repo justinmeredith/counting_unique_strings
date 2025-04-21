d3353fbecb57379b545251a863e5c5396b17fe60
justinmeredith <jm.4444@icloud.com>
2025-04-20 22:15:38 -0500

    Initial commit.

---
+++ counting_unique_strings.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+struct UniqueString {
+    string the_unique_string;
+    int number_of_appearances;
+};
+
+int main() {
+
+    return 0;
+}
